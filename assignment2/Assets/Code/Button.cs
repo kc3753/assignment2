4d782341997d3e1cdc03afd50043aad69bf702d7
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2021-11-11 23:00:44 -0500

    Update Button.cs

--- assignment2/Assets/Code/Button.cs
+++ assignment2/Assets/Code/Button.cs
@@ -8,16 +8,19 @@ public class Button : MonoBehaviour
     //public GameObject activated;
     public GameObject buttonGameObjectLink;
     activatable buttonLink; 
-
+    float up;
+    float down;
     void Start(){
         buttonLink = buttonGameObjectLink.GetComponent<activatable>();
+        up = transform.position.y;
+        down = transform.position.y - 0.05f;
     }
     private void OnTriggerEnter(Collider other)
         {
             
             if(other.gameObject.CompareTag("Player0") || other.gameObject.CompareTag("Player1") )
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+                transform.position = new Vector3(transform.position.x, down, transform.position.z);
                 transform.GetChild(0).GetComponent<Light>().enabled = true;
                 buttonLink.activate();
                 //activated.SetActive(false);
@@ -28,7 +31,7 @@ public class Button : MonoBehaviour
         {
             if(other.gameObject.CompareTag("Player0") || other.gameObject.CompareTag("Player1") )
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+                transform.position = new Vector3(transform.position.x, up, transform.position.z);
                 transform.GetChild(0).GetComponent<Light>().enabled = false;
                 buttonLink.deactivate();
                 //activated.SetActive(true);
